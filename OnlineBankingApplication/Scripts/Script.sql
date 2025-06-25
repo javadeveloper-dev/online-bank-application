df9ba3cef16c64964092fbbadc0b29aa7526e3aa
javadeveloper-dev <102380526+javadeveloper-dev@users.noreply.github.com>
2025-06-25 11:43:29 +0530

    Update Script.sql
    Removing Unnnecessay Space

--- OnlineBankingApplication/Scripts/Script.sql
+++ OnlineBankingApplication/Scripts/Script.sql
@@ -1,11 +1,9 @@
-
-
-
 select * from banking_app.admin_register;
 
 alter table banking_app.admin_register modify profile_photo mediumblob;
 
 desc banking_app.admin_register;
+
 truncate table banking_app.admin_register; where id=2;
 
 delete * from banking_app.admin_register where id=27;
